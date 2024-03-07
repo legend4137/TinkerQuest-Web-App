895d04997f89e3128d8d7e431237c434583efc0a
THARAK HEGDE <b22mt044@iitj.ac.in>
2024-03-07 21:14:26 +0530

    Built Basic React and Navbar and Routes
    basic Navbar with basic designs

---
+++ my_app/src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <div>Hi there this is tharak here!</div>
+        <Footer/>
+    </div>
+  )
+}
---
+++ my_app/src/screens/Tests.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function Tests() {
+  return (
+    <div>
+    <Navbar/>
+    <div>Tests</div>
+    </div>
+  
+  )
+}
